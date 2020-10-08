4a56a5865f213518d333276e8c27187cc8aa89d0
Administrator <Administrator@DESKTOP-UNAQLM1>
2020-10-08 09:14:59 +0800

    郭爽控制器

---
+++ Controllers/JctionController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Flying_Cow_TMSAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Dapper;
+using Microsoft.AspNetCore.Cors;
+
+namespace Flying_Cow_TMSAPI.Controllers
+{
+    [Produces("application/json", "application/xml")]
+    [Route("api/[controller]")]
+    [EnableCors("cors")]//设置跨域处理的代理
+    [ApiController]
+    public class JctionController : ControllerBase
+    {
+        public TMSDBContext db;
+        public JctionController(TMSDBContext db) { this.db = db; }
+        private string con = "Data Source=10.3.158.51;Initial Catalog=TMSDB;uid=sa;pwd=123";
+        [Route("RoleLogin")]
+        [HttpGet]
+        public async Task<IEnumerable<Role>> Show(string name)
+        {
+            var list = from a in db.UserInfo
+                       from b in db.UserRole
+                       from c in db.Role
+                       where a.user_Id == b.userid && b.roleid == c.role_Id && a.user_Name == name
+                       select new Role { role_Id = b.roleid };
+            return await list.ToListAsync();
+        }
+        [Route("S")]
+        [HttpGet]
+        public List<Jurisdiction> RoleJue(string name="")
+        {
+            using (IDbConnection a = new SqlConnection(con))
+            {
+
+                string sql = $"select * from UserInfo a join UserRole b on a.user_Id = b.userid join Role c on b.roleid = c.role_Id where a.user_Name='{name}'";//根据用户名获取数据
+                var list = a.Query<Role>(sql).FirstOrDefault().role_Id;
+                int id = list;
+                string str = $"select Jurisdiction.juris_Id,Jurisdiction.juris_MenuName from Role join RoleJurisdiction on Role.role_Id=RoleJurisdiction.roleid join Jurisdiction on RoleJurisdiction.jurisid=Jurisdiction.juris_Id where Role.role_Id='{id}' and juris_Pid=0";//根据ID查询权限
+                return a.Query<Jurisdiction>(str).ToList();
+            }
+        }
+        /// <summary>
+        /// 二级菜单
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("Two")]
+        [HttpGet]
+        public List<Jurisdiction> EnumTwo(int ID)
+        {
+            using (IDbConnection a = new SqlConnection(con))
+            {
+                string sql = $"select * from Jurisdiction where juris_Pid='{ID}'";
+                var list= a.Query<Jurisdiction>(sql).ToList();
+                return list;
+            }
+        }
+        [Route("TwoDian")]
+        [HttpGet]
+        public List<Jurisdiction> Dian(int id)
+        {
+            using IDbConnection a = new SqlConnection(con);
+            //Role 角色表 Jurisdiction菜单表  RoleJurisdiction角色菜单表
+            string sql2 = $"select b.role_Id from Role b join RoleJurisdiction c on b.role_Id=c.roleid join Jurisdiction d on c.jurisid=d.juris_Id where d.juris_Pid = '{id}'";
+            int Pid = a.Query<Role>(sql2).FirstOrDefault().role_Id;
+            string sql = $"select d.juris_Id,d.juris_MenuName,d.juris_Pid,juris_MenuUrl from Role b join RoleJurisdiction c on b.role_Id=c.roleid join Jurisdiction d on c.jurisid=d.juris_Id where 1=1";
+            sql += $" and b.role_Id='{Pid}'and d.juris_Pid='{id}'";
+            return a.Query<Jurisdiction>(sql).ToList();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flying_Cow_TMSAPI.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flying_Cow_TMSAPI.Controllers
+{
+    [Produces("application/json", "application/xml")]
+    [Route("api/[controller]")]
+    [EnableCors("cors")]//设置跨域处理的代理
+    public class LoginController : ControllerBase
+    {
+        public TMSDBContext db;
+        public LoginController(TMSDBContext db) { this.db = db; } 
+
+        //[Route("Login")]
+        //[HttpGet]
+        //public async Task<ActionResult<int>> Login(string name, string mm)
+        //{
+        //    return await db.UserInfo.Where(s => s.user_Name.Equals(name) && s.user_PassWord.Equals(mm)).CountAsync();
+        //}
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult<int>> Login(string name, string mm)
+        {
+            return await db.UserInfo.Where(e => e.user_Name.Equals(name) && e.user_PassWord.Equals(mm)).CountAsync();
+        }
+        /// <param name="request"></param>
+        /// <returns></returns>
+        //public int AddUserInfo(UserInfo user)
+        //{
+        //    int line = 0;
+        //    if (!string.IsNullOrEmpty(user.UserName) && !string.IsNullOrEmpty(user.UserPwd))
+        //    {
+        //        user.CreateTime = DateTime.Now;
+        //        line = userIDal.AddUserInfo(user);
+        //    }
+        //    return line;
+        //}
+        //注册
+        public async Task<ActionResult<int>> enroll(string name,string pwd,string confirm)
+        {
+            UserInfo u = new UserInfo();
+            u.user_Name = name;
+            u.user_PassWord = pwd;
+            u.user_Confirmpwd = confirm;
+            db.UserInfo.Add(u);
+            return await db.SaveChangesAsync();
+        }
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="uname"></param>
+        /// <param name="rname"></param>   
+        /// <param name="jmenuURL"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Sel")]
+        public async Task<ActionResult<IEnumerable<Jurisdiction>>> Sel(string uname,string rname,string jmenuURL )
+        {
+            int id = ( from A in db.UserInfo
+                       join B in db.UserRole on A.user_Id equals B.userid
+                       join C in db.Role on B.roleid equals C.role_Id
+                       join D in db.RoleJurisdiction on C.role_Id equals D.roleid
+                       join E in db.Jurisdiction on D.jurisid equals E.juris_Id
+                       where A.user_Name == uname
+                        select new UserInfo()
+                        {
+                             jurisid=D.jurisid,
+                             juris_Id =E.juris_Id,
+                             juris_MenuName=E.juris_MenuName,
+                             juris_MenuUrl=E.juris_MenuUrl,
+                             juris_Pid=E.juris_Pid,
+                             roleid=B.roleid,
+                             rolej_Id=D.rolej_Id, 
+                             role_Id=C.role_Id,
+                             role_Name=C.role_Name,
+                             userid=B.userid,
+                             user_Id=A.user_Id,
+                             user_Name=A.user_Name,
+                             user_PassWord=A.user_PassWord,
+                             user_Phone=A.user_Phone,
+                             usro_Id=B.usro_Id
+                        }).FirstOrDefault().juris_Id;
+            var list = from j in db.Jurisdiction where j.juris_Id == id select j;
+            return await list.ToListAsync();
+        }
+    }
+}
